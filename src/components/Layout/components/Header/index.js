1b992408ed7d66c54aec266e88ab7c98439f0969
hieunam2k4 <hieunam492004@gmail.com>
2023-11-20 11:47:18 +0700

    Build UI Header #5

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -17,25 +17,51 @@ import images from '~/assets/image/index';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '../AccountItem';
 import Button from '~/components/Button';
-import Menu from '~/components/Popper/Menu'
+import Menu from '~/components/Popper/Menu';
+
+//Handle Logic
+const handleMenuChange = (menuItem) => {
+    switch(menuItem.type) {
+        case 'language':
+            //Handle change language
+            console.log(menuItem);
+            break
+        default:
+
+    }
+}
 
 const cx = classNames.bind(styles);
 const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {   
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {   
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tieng Viet',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
         title: 'Feedback and help',
-        to: '/feedback'
+        to: '/feedback',
     },
     {
         icon: <FontAwesomeIcon icon={faKeyboard} />,
         title: 'Keyboard shortcut',
     },
-
-]
+];
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
@@ -80,9 +106,7 @@ function Header() {
                 <div className={cx('actions')}>
                     <Button text>Register</Button>
                     <Button primary>Log in</Button>
-                    <Menu
-                        items={MENU_ITEMS}
-                    >
+                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
                         </button>
